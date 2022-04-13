e0c00360a9e5f83a6fa39afc61fc5ed5ca0a4f22
Scarlett Adams <scarlettadams@Scarletts-Work-MacBook-Pro.local>
2022-04-13 09:46:07 +0100

    complete the solution and the test for day-1/2017

---
+++ 2017/day1/__tests__/produceCaptcha.test.js
@@ -0,0 +1,22 @@
+const produceCaptcha = require("../produceCaptcha");
+
+describe("produceCaptcha", () => {
+  it("should return 0 when passes an empty string", () => {
+    expect(produceCaptcha("")).toBe(0);
+  });
+  it("should return the repeated number when passes a string with 2 different digits", () => {
+    expect(produceCaptcha("12")).toBe(0);
+  });
+  it("should return the sum of the continuous repeated number when passes a string with some repeated digits in it", () => {
+    expect(produceCaptcha("1122")).toBe(3);
+    expect(produceCaptcha("112233")).toBe(6);
+  });
+  it("should return the sum of the repeated number when passes a number with the same number for both the first and the last digit", () => {
+    expect(produceCaptcha("1111")).toBe(4);
+    expect(produceCaptcha("91212129")).toBe(9);
+  });
+  it("should return 0r when passes a number without any repeated digits", () => {
+    expect(produceCaptcha("1234")).toBe(0);
+    expect(produceCaptcha("12")).toBe(0);
+  });
+});
---
+++ 2017/day1/produceCaptcha.js
@@ -0,0 +1,19 @@
+const puzzleInput = require("./puzzle-input");
+
+const produceCaptcha = (numStr) => {
+  let sum = 0;
+  if (numStr.length < 2) return 0;
+
+  for (let i = 0; i < numStr.length - 1; i++) {
+    if (numStr[i] === numStr[i + 1]) sum += parseInt(numStr[i + 1]);
+  }
+  if (numStr.length > 2 && numStr[numStr.length - 1] === numStr[0])
+    sum += parseInt(numStr[0]);
+
+  return sum;
+};
+
+const result = produceCaptcha(puzzleInput);
+console.log(result, "<<<<<<<<<<<<result");
+
+module.exports = produceCaptcha;
